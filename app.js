238f9531d1188fe5fdf961b6e0655f55a67936d2
Nikita Mamaev <palmally@yandex.ru>
2022-10-05 22:07:56 +0300

    test celebrate

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const { celebrate, Joi } = require('celebrate');
+const { celebrate, errors } = require('celebrate');
 const cardsRouter = require('./routes/cardsRouter');
 const usersRouter = require('./routes/usersRouter');
 const auth = require('./middlewares/auth');
 const errorsHandler = require('./middlewares/errorsHandler');
 const { login, createUser } = require('./controllers/user');
-const { NOT_FOUND_ERROR_CODE } = require('./utills/constants');
+const NotFoundError = require('./errors/notFoundError');
+const { userSchema } = require('./utills/constants');
 
 const { PORT = 3000 } = process.env;
 
@@ -18,13 +19,14 @@ app.use(cookieParser());
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signup', celebrate(userSchema), createUser);
 app.use(auth);
 app.use('/cards', cardsRouter);
 app.use('/users', usersRouter);
-app.use('*', (_, res) => {
-  res.status(NOT_FOUND_ERROR_CODE).send({ message: 'Ресурс не найден. Проверьте URL и метод запроса' });
+app.use('*', () => {
+  throw new NotFoundError('Ресурс не найден. Проверьте URL и метод запроса');
 });
+app.use(errors());
 app.use(errorsHandler);
 
 app.listen(PORT, () => {
--- utills/constants.js
+++ utills/constants.js
@@ -4,3 +4,14 @@ module.exports.NOT_FOUND_ERROR_CODE = 404;
 module.exports.DEFAULT_ERROR_CODE = 500;
 module.exports.SUCCESS_CREATED_CODE = 201;
 module.exports.URL_REGEXP = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
+const { celebrate, Joi } = require('celebrate');
+
+module.exports.userSchema = {
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().uri(),
+  }),
+};

7cec8cc4ce67e1d93a27e62f04224d58adbec148
Nikita Mamaev <palmally@yandex.ru>
2022-10-09 23:47:39 +0300

    test auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,14 +5,14 @@ const { JWT_SECRET } = require('../utills/constants');
 module.exports = (req, _, next) => {
   const { token } = req.cookies;
   if (!token) {
-    throw new UnauthorizedError('Необходима авторизация');
+    throw new UnauthorizedError('Необходима авторизация тест1');
   }
   let payload;
 
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    throw new UnauthorizedError('Необходима авторизация');
+    throw new UnauthorizedError('Необходима авторизация тест2');
   }
   req.user = payload;
   next();
